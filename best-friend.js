fd182babd6542af1f12a7f3906b2ca42b2afc213
Nahid-07 <nahidasif1998@gmail.com>
2022-08-09 11:56:39 +0600

    problem solved

---
+++ best-friend.js
@@ -0,0 +1,15 @@
+/* ৪. একটা ফাংশন লিখবে। এই ফাংশনের নাম হবে bestFriend তারপর সেই ফাংশনে ইনপুট প্যারামিটার হিসেবে একটা array নিবে। সেই array এর মধ্যে তোমার সব ফ্রেন্ডের নাম থাকবে। এখন তোমার কাজ হচ্ছে যে ফ্রেন্ড এর নাম সবচেয়ে বড় সেই ফ্রেন্ড এর নাম রিটার্ন করে দেয়া। এই ক্ষেত্রে তুমি নামটা অর্থাৎ ফ্রেন্ডের নাম (স্ট্রিং) রিটার্ন করতে হবে */
+
+function bestFriend(array){
+    let largest = array[0];
+    for(i=0;i<array.length;i++){
+        let element = array[i];
+        if(element.length>largest.length){
+            largest = element;
+        }
+    }
+    return largest;
+}
+let myArray = ["Nahid Hassan Asif", "Asif", "Nahidul", "Nahidul Islam Asif"]
+let result = bestFriend(myArray);
+console.log(result);
